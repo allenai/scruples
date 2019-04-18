99bdc0b85d21db28940a2076a5ad65bcd3ed630d
Nicholas Lourie <nicholasl@allenai.org>
2019-04-18 16:05:28 -0700

    Add a tests package with integration to setup.py
    Move socialnorms into a the src/ directory, add the tests package
    and setup tests to be run with setup.py.

--- setup.py
+++ setup.py
@@ -29,7 +29,33 @@ setup(
     ],
     license='Apache',
     packages=['socialnorms'],
-    install_requires=requirements,
+    package_dir={'': 'src'},
+    install_requires=[
+        'Click >= 7.0',
+        'apex >= 0.1',
+        'attrs >= 19.1.0',
+        'ftfy >= 5.5.1',
+        'numpy >= 1.16.2',
+        'pytorch-pretrained-bert >= 0.6.1',
+        'regex >= 2018.1.10',
+        'scikit-learn >= 0.20.3',
+        'scikit-optimize >= 0.5.2',
+        'scipy >= 1.2.1',
+        'spacy >= 2.1.3',
+        'tensorboard >= 1.13.1',
+        'tensorboardX >= 1.6',
+        'tensorflow-estimator >= 1.13.0',
+        'tensorflow >= 1.13.1',
+        'torch >= 1.0.1.post2',
+        'torchvision >= 0.2.2.post3',
+        'xgboost >= 0.82'
+    ],
+    setup_requires=[
+        'pytest-runner'
+    ],
+    tests_require=[
+        'pytest'
+    ],
+    include_package_data=True,
     python_requires='>= 3.7',
-    zip_safe=False
-)
+    zip_safe=False)
