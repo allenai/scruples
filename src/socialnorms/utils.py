eb3c43663e865abe9cf28991c73e642b1e0d50d9
Nicholas Lourie <nicholasl@allenai.org>
2019-04-18 16:38:30 -0700

    Fix bug in configure_logging
    Because configure_logging relied on calling basicConfig, it did
    not work well when log handlers had already been setup by other
    code. Setting up the log handler manually deals with this issue.
    Also, by now returning the log handler, it makes the function
    easier to test.

--- src/socialnorms/utils.py
+++ src/socialnorms/utils.py
@@ -15,8 +15,8 @@ from sklearn import metrics
 from . import settings
 
 
-def configure_logging(verbose: bool = False):
-    """Configure logging.
+def configure_logging(verbose: bool = False) -> logging.Handler:
+    """Configure logging and return the log handler.
 
     This function is useful in scripts when logging should be set up
     with a basic configuration.
@@ -26,11 +26,21 @@ def configure_logging(verbose: bool = False):
     verbose : bool, optional (default=False)
         If ``True``, set the log level to DEBUG, else set the log level
         to INFO.
+
+    Returns
+    -------
+    logging.Handler
+        The log handler set up by this function to handle basic logging.
     """
-    logging.basicConfig(
-        level=logging.DEBUG if verbose else logging.INFO,
-        format=settings.LOG_FORMAT,
-        handlers=[logging.StreamHandler()])
+    # set up the log handler
+    handler = logging.StreamHandler()
+    handler.setLevel(logging.DEBUG if verbose else logging.INFO)
+    handler.setFormatter(logging.Formatter(settings.LOG_FORMAT))
+
+    # attach the log handler to root
+    logging.root.addHandler(handler)
+
+    return handler
 
 
 _character_filter_regex = regex.compile(r'[^\w\s]')
