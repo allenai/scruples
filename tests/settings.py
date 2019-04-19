f73d905f4bf6bf7a9d4dcdf83debb6f159605a0f
Nicholas Lourie <nicholasl@allenai.org>
2019-04-19 14:40:14 -0700

    Add test comment and test post fixtures

--- tests/settings.py
+++ tests/settings.py
@@ -4,6 +4,10 @@
 # fixture locations
 
 SOCIALNORMS_EASY_PATH = 'fixtures/socialnorms-easy'
+
 SOCIALNORMS_EASY_TRAIN_PATH = 'fixtures/socialnorms-easy/train.jsonl'
 SOCIALNORMS_EASY_DEV_PATH = 'fixtures/socialnorms-easy/dev.jsonl'
 SOCIALNORMS_EASY_TEST_PATH = 'fixtures/socialnorms-easy/test.jsonl'
+
+TEST_COMMENT_PATH = 'fixtures/raw-data/test-comment.jsonl'
+TEST_POST_PATH = 'fixtures/raw-data/test-post.jsonl'
