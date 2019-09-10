9cb1ee42ac4cf4ff5e23984862189b5a6828da0f
Nicholas Lourie <nicholasl@allenai.org>
2019-09-10 15:22:42 -0700

    Add a project method to LatentTraitModel

--- src/scruples/analysis/traits.py
+++ src/scruples/analysis/traits.py
@@ -101,7 +101,7 @@ class LatentTraitModel:
                 requires_grad=True,
                 device=device))
 
-        # loss and optimization
+        # optimization
         optimizer = th.optim.SGD([W, b], lr=lr)
         scheduler = th.optim.lr_scheduler.ReduceLROnPlateau(
             optimizer=optimizer,
@@ -160,5 +160,136 @@ class LatentTraitModel:
         self.n_samples_ = n_samples
         self.n_variables_ = n_variables
 
-        self.W_ = W
-        self.b_ = b
+        self.W_ = W.detach().cpu()
+        self.b_ = b.detach().cpu()
+
+    def project(
+            self,
+            data: th.Tensor,
+            lr: float = 1e1,
+            n_batch_size: int = 128,
+            patience: int = 5,
+            n_epochs: int = 250,
+            device: th.device = th.device('cpu')
+    ) -> th.Tensor:
+        """Project ``data`` into the latent space.
+
+        Project ``data`` into the latent space using the maximum a
+        posteriori (MAP) estimate.
+
+        Parameters
+        ----------
+        data : th.Tensor
+            The data matrix to project into the latent space. The tensor
+            should have the shape: # samples x # observed variables.
+        lr : float, optional (default=1e1)
+            The learning rate for SGD.
+        n_batch_size : int, optional (default=128)
+            The mini-batch size for SGD.
+        patience : int, optional (default=5)
+            The number of epochs to wait for the loss to reduce before
+            reducing the learning rate.
+        n_epochs : int, optional (default=250)
+            The number of epochs to run SGD.
+        device : th.device, optional (default=th.device('cpu'))
+            The torch device to use.
+
+        Returns
+        -------
+        th.Tensor
+            ``data`` projected into the latent space. This tensor has
+            the shape: # samples x # latent traits.
+        """
+        if self.latent_dim == 0:
+            raise ValueError(
+                'Cannot project the observations into the latent space'
+                ' when the latent dimension is 0.')
+
+        # By Bayes' Rule, we have:
+        #
+        #                P(Y | Z) P(Z)
+        #     P(Z | Y) = -------------
+        #                    P(Y)
+        #
+        # Since P(Y) is a constant, we can then obtain the MAP estimate
+        # by optimizing Z such that P(Y | Z) P(Z) is maximized, or
+        # equivalently such that - (log P(Y | Z) + log P(Z)) is
+        # minimized.
+        #
+        # Applying this to the likelihood yields:
+        #
+        #     - log \prod_n P(y_n | z_n) P(z_n)
+        #     = - \sum_n \log P(y_n | z_n) + log P(z_n)
+        #     = - \sum_n \sum_i y_{ni} \log \sigma(w_i z_n + b_i)
+        #                       + (1 - y_{ni}) \log (1 - \sigma(w_i z_n + b_i))
+        #                - \frac{|| z_n ||_2^2}{2}
+        #                + C
+        #
+        # Where C is a constant.
+
+        # move the data to the device
+        data = th.tensor(data, device=device)
+
+        # compute constants
+        n_samples, n_variables = data.shape
+
+        if n_variables != self.n_variables_:
+            raise ValueError(
+                'The data matrix has a different number of observed'
+                ' variables than the model was trained with.')
+
+        W = self.W_.to(device)
+        b = self.b_.to(device)
+
+        # initialize latent variable estimates
+        # shape: n_samples x self.latent_dim
+        zs = th.nn.Parameter(
+            th.zeros(
+                n_samples,
+                self.latent_dim,
+                requires_grad=True,
+                device=device))
+
+        # optimization
+        optimizer = th.optim.SGD([zs], lr=lr)
+        scheduler = th.optim.lr_scheduler.ReduceLROnPlateau(
+            optimizer=optimizer,
+            mode='min',
+            factor=1e-1,
+            patience=patience)
+
+        # run training
+        for epoch in range(n_epochs):
+            # nlp stands for negative log-probability
+            epoch_nlps = []
+            epoch_perm = np.random.permutation(len(data))
+            for i in range(0, len(data), n_batch_size):
+                optimizer.zero_grad()
+
+                # assemble the mini-batch
+                # shape: n_batch_size x self.latent_dim
+                mb_latents = zs[epoch_perm[i:i+n_batch_size]]
+                # shape: n_batch_size x n_variables
+                mb_labels = data[epoch_perm[i:i+n_batch_size]]
+
+                # shape: n_batch_size x n_variables
+                mb_probs = th.sigmoid(th.matmul(mb_latents, W) + b)
+
+                # shape: scalar
+                mb_nlp = - th.mean(
+                    th.sum(
+                        mb_labels.float() * th.log(mb_probs)
+                        + (1 - mb_labels.float()) * th.log(1 - mb_probs),
+                        dim=-1)
+                    - th.matmul(mb_latents, mb_latents.t()) / 2)
+
+                mb_nlp.backward()
+                optimizer.step()
+
+                # update statistics
+                epoch_nlps.append(mb_nlp.cpu().item())
+
+            # update the learning rate
+            scheduler.step(np.mean(epoch_nlps))
+
+        return zs.detach().cpu()
--- tests/scruples/analysis/test_traits.py
+++ tests/scruples/analysis/test_traits.py
@@ -54,14 +54,14 @@ class LatentTraitModelTestCase(unittest.TestCase):
 
         # evaluate the parameter estimates
 
-        W_hat = model.W_.detach().cpu().numpy()
+        W_hat = model.W_.numpy()
         # Since the parameters in a latent trait / factor analysis model
         # are only determined up to a rotation, we must align the
         # learned parameters with the ground-truth with a rotation.
         R, _ = orthogonal_procrustes(W_hat, W)
         W_hat = np.matmul(W_hat, R)
 
-        b_hat = model.b_.detach().cpu().numpy()
+        b_hat = model.b_.numpy()
 
         # check that self.n_samples_ and self.n_variables_ were set
         # correctly
@@ -86,6 +86,63 @@ class LatentTraitModelTestCase(unittest.TestCase):
                 7.5e-2)
         self.assertLess(
             np.mean((b - b_hat)**2),
+            7.5e-2)
+
+    def _check_project_on_synthetic_example(
+            self,
+            latent_dim: int,
+            n_samples: int,
+            n_variables: int,
+            lr: float,
+            n_batch_size: int,
+            patience: int,
+            n_epochs: int
+    ) -> None:
+        # create a random ground-truth for W and b
+        W = np.random.randn(latent_dim, n_variables)
+        b = np.random.randn(n_variables)
+
+        # generate data from the ground-truth model
+
+        # step 1: sample the latent variables
+        zs = np.random.randn(n_samples, latent_dim)
+        # step 2: use the latent variables to generate the observations
+        data = (
+            # evaluate the probability vectors
+            1. / (1. + np.exp(-(np.matmul(zs, W) + b)))
+            # compare the probabilities to uniform random noise to
+            # create the observations
+            > np.random.rand(n_samples, n_variables)
+        ).astype(int)
+
+        # create the model and fit it artificially
+        model = traits.LatentTraitModel(latent_dim=latent_dim)
+        model.n_samples_ = n_samples
+        model.n_variables_ = n_variables
+        model.W_ = th.tensor(W).float()
+        model.b_ = th.tensor(b).float()
+
+        # project the observations into the latent space
+        zs_hat = model.project(
+            data=data,
+            lr=lr,
+            n_batch_size=n_batch_size,
+            patience=patience,
+            n_epochs=n_epochs,
+            device=th.device('cuda')
+              if th.cuda.is_available()
+              else th.device('cpu')).numpy()
+
+        # sanity-check that the projected observations give better
+        # estimates than the origin
+        self.assertLess(
+            np.mean((zs - zs_hat)**2),
+            np.mean(zs**2))
+
+        # check that the projected observations give good estimates
+        # for this easy problem
+        self.assertLess(
+            np.mean((zs - zs_hat)**2),
             7.5e-2)
 
     def test___init__(self):
@@ -126,3 +183,30 @@ class LatentTraitModelTestCase(unittest.TestCase):
             n_batch_size=1024,
             patience=3,
             n_epochs=25)
+
+    def test_project_on_no_trait_model(self):
+        with self.assertRaises(ValueError):
+            traits.LatentTraitModel(latent_dim=0).project(
+                th.randn(100, 20))
+
+    @pytest.mark.slow
+    def test_project_on_one_trait_model(self):
+        self._check_project_on_synthetic_example(
+            latent_dim=1,
+            n_samples=256,
+            n_variables=256,
+            lr=1e0,
+            n_batch_size=128,
+            patience=5,
+            n_epochs=250)
+
+    @pytest.mark.slow
+    def test_project_on_two_trait_model(self):
+        self._check_project_on_synthetic_example(
+            latent_dim=2,
+            n_samples=256,
+            n_variables=256,
+            lr=1e0,
+            n_batch_size=128,
+            patience=5,
+            n_epochs=250)
